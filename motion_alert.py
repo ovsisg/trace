c18f0ede39d574117f64aa89880dbc882060ff32
ovsisg <ovsisg@gmail.com>
2026-08-18 22:45:40 +0200

    Add grayscale and Gaussian blur

--- motion_alert.py
+++ motion_alert.py
@@ -4,7 +4,9 @@ video = cv2.VideoCapture(0) # Open the webcam
 
 while True:
     check, frame = video.read() # Ask the webcam for the next frame
-    cv2.imshow("Webcam", frame) # Display the frame in a window
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert the frame from colour to grayscale
+    blurred_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0) 
+    cv2.imshow("Webcam", blurred_frame) # Display the frame in a window
 
     key = cv2.waitKey(1) # Wait 1 millisecond for a key press
 
